f52f1ea99cb150ec95f43715a5dcef7fc44017f2
jhjdl3984 <jhjdl3984@gmail.com>
2025-12-10 10:44:26 +0900

    스키마

--- app/models/question.py
+++ app/models/question.py
@@ -11,11 +11,22 @@
 from tortoise import fields
 from tortoise.models import Model
 
+# class Category(Model):
+#     id = fields.IntField(pk=True)
+#     name = fields.CharField(max_length=50, unique=True)
+#
+#     class Meta:
+#         table = "categories"
 
-class Question(Model):
-    id = fields.IntField(pk=True)
-    text = fields.CharField(max_length=30)
-    category = fields.CharField(max_length=15)
+# class Question(Model):
+#     id = fields.IntField(pk=True)
+#     text = fields.CharField(max_length=30)
+#     category = fields.CharField(max_length=15)
+#     # categories: fields.ManyToManyRelation["Category"] = fields.ManyToManyField(
+#     #     "models.Category",
+#     #     related_name="questions",
+#     #     through="question_categories"
+#     # )
 
-    class Meta:
-        table = "questions"
+    # class Meta:
+    #     table = "questions"
